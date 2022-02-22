2f5545d2b7952c7c52c700510df5a820fa8daefc
den-taku <dentaku1412@gmail.com>
2022-02-23 02:16:14 +0900

    Implement CPU

--- ch03/src/main.rs
+++ ch03/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut cpu = CPU {
+    let mut cpu = Cpu {
         registers: [0; 16],
         memory: [0; 4096],
         position_in_memory: 0,
@@ -28,7 +28,7 @@ fn main() {
     println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
 }
 
-struct CPU {
+struct Cpu {
     registers: [u8; 16],
     position_in_memory: usize,
     memory: [u8; 0x1000],
@@ -36,7 +36,7 @@ struct CPU {
     stack_pointer: usize,
 }
 
-impl CPU {
+impl Cpu {
     fn read_opcode(&self) -> u16 {
         let p = self.position_in_memory;
         let op_byte1 = self.memory[p] as u16;
@@ -50,26 +50,68 @@ impl CPU {
             let opcode = self.read_opcode();
             self.position_in_memory += 2;
 
-            let c = ((opcode & 0xF000) >> 12) as u8;
             let x = ((opcode & 0x0F00) >> 8) as u8;
             let y = ((opcode & 0x00F0) >> 4) as u8;
-            let d = ((opcode & 0x000F) >> 0) as u8;
+            let op_minor = (opcode & 0x000F) as u8;
 
-            let nnn = opcode & 0x0FFF;
-            // let kk = (opcode & 0x00FF) as u8;
+            let addr = opcode & 0x0FFF;
+            let kk = (opcode & 0x00FF) as u8;
 
-            match (c, x, y, d) {
-                (0, 0, 0, 0) => {
+            match opcode {
+                0x0000 => {
                     return;
                 }
-                (0, 0, 0xE, 0xE) => self.ret(),
-                (0x2, _, _, _) => self.call(nnn),
-                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                0x00E0 => { /* CLEAR SCREEN */ }
+                0x00EE => self.ret(),
+                0x1000..=0x1FFF => self.jmp(addr),
+                0x2000..=0x2FFF => self.call(addr),
+                0x3000..=0x3FFF => self.se(x, kk),
+                0x4000..=0x4FFF => self.sne(x, kk),
+                0x5000..=0x5FFF => self.se(x, y),
+                0x6000..=0x6FFF => self.ld(x, kk),
+                0x7000..=0x7FFF => self.add(x, kk),
+                0x8000..=0x8FFF => match op_minor {
+                    0 => self.ld(x, self.registers[y as usize]),
+                    1 => self.or_xy(x, y),
+                    2 => self.and_xy(x, y),
+                    3 => self.xor_xy(x, y),
+                    4 => self.add_xy(x, y),
+                    _ => todo!("opcode: {opcode:04x}"),
+                },
                 _ => todo!("opcode {opcode:04x}"),
             }
         }
     }
 
+    /// (6xkk) LD sets the value `kk` into register `vx`
+    fn ld(&mut self, vx: u8, kk: u8) {
+        self.registers[vx as usize] = kk;
+    }
+
+    /// (7xkk) Add the value `kk` into register `vx`
+    fn add(&mut self, vx: u8, kk: u8) {
+        self.registers[vx as usize] += kk;
+    }
+
+    fn se(&mut self, vx: u8, kk: u8) {
+        if vx == kk {
+            self.position_in_memory += 2;
+        }
+    }
+
+    /// () SNE Store if Not Equal
+    fn sne(&mut self, vx: u8, kk: u8) {
+        if vx != kk {
+            self.position_in_memory += 2;
+        }
+    }
+
+    /// (1nnn) JUMP to `addr`
+    fn jmp(&mut self, addr: u16) {
+        self.position_in_memory = addr as usize;
+    }
+
+    /// (2nnn) CALL sub-rotine at `addr`
     fn call(&mut self, addr: u16) {
         let sp = self.stack_pointer;
         let stack = &mut self.stack;
@@ -83,6 +125,7 @@ impl CPU {
         self.position_in_memory = addr as usize;
     }
 
+    /// (00ee) RET return from the current sub-routiine
     fn ret(&mut self) {
         if self.stack_pointer == 0 {
             panic!("Stack underflow!")
@@ -93,6 +136,7 @@ impl CPU {
         self.position_in_memory = call_addr as usize;
     }
 
+    /// (7xkk)
     fn add_xy(&mut self, x: u8, y: u8) {
         let arg1 = self.registers[x as usize];
         let arg2 = self.registers[y as usize];
@@ -105,5 +149,17 @@ impl CPU {
         } else {
             self.registers[0xF] = 0;
         }
+    }
+
+    fn and_xy(&mut self, x: u8, y: u8) {
+        self.registers[x as usize] &= self.registers[y as usize];
+    }
+
+    fn or_xy(&mut self, x: u8, y: u8) {
+        self.registers[x as usize] |= self.registers[y as usize];
+    }
+
+    fn xor_xy(&mut self, x: u8, y: u8) {
+        self.registers[x as usize] ^= self.registers[y as usize];
     }
 }
